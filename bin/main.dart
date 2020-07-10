f2ba2fa08d440debd54d4debc042778062685ffa
AlexBismuth <bismuthalex@gmail.com>
2020-07-09 17:01:43 -0700

    i_am_rich_AppBrewery

--- bin/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:http/http.dart';
-import 'package:school_web_scraper/school_web_scraper.dart' as school_web_scraper;
-
-void main(List<String> arguments) async 
-{
-  //print(await school_web_scraper.initiate(Client()));
-  await school_web_scraper.WebsiteHasChanged(Client());
-}
