60bb0ef6d4c67748f501b0415ef8eaaec5db7c27
AlexBismuth <bismuthalex@gmail.com>
2020-07-10 15:25:12 -0700

    Added a container!

--- business_card/lib/main.dart
+++ business_card/lib/main.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  //Q: What does this do? const MyApp({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.pink,
+        body: SafeArea(
+          child: Container(
+            height: 50.0,
+            width: 100.0,
+            color: Colors.white,
+            child: Text('hello'),
+            alignment: Alignment.center,
+            margin: EdgeInsets.all(10.0),
+            padding: EdgeInsets.all(10.0),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
