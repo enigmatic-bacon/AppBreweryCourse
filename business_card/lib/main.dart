ab5e3e0dad9129692790bcc7963ada175ad2ba3b
AlexBismuth <bismuthalex@gmail.com>
2020-07-10 15:47:37 -0700

    App challenge complete!

--- business_card/lib/main.dart
+++ business_card/lib/main.dart
@@ -10,42 +10,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.pink,
+        backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            verticalDirection: VerticalDirection.down,
             children: <Widget>[
               Container(
-                height: 50.0,
                 width: 100.0,
-                color: Colors.white,
-                child: Text('Container 1'),
-                alignment: Alignment.center,
-                margin: EdgeInsets.all(10.0),
-                padding: EdgeInsets.all(10.0),
+                color: Colors.red,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    color: Colors.green,
+                  ),
+                ],
               ),
               Container(
-                height: 50.0,
                 width: 100.0,
-                color: Colors.yellow,
-                child: Text('Container 2'),
-                alignment: Alignment.center,
-                margin: EdgeInsets.all(10.0),
-                padding: EdgeInsets.all(10.0),
-              ),
-              Container(
-                height: 50.0,
-                width: 100.0,
-                color: Colors.white,
-                child: Text('Container 3'),
-                alignment: Alignment.center,
-                margin: EdgeInsets.all(10.0),
-                padding: EdgeInsets.all(10.0),
-              ),
-              Container(
-                width: double.infinity,
+                color: Colors.blue,
               ),
             ],
           ),
