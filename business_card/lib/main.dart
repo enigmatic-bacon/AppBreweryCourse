109261c47caf70982f72e212e1954ec2b1c895ca
AlexBismuth <bismuthalex@gmail.com>
2020-07-10 15:41:16 -0700

    Playing with row and column properties

--- business_card/lib/main.dart
+++ business_card/lib/main.dart
@@ -12,14 +12,42 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.pink,
         body: SafeArea(
-          child: Container(
-            height: 50.0,
-            width: 100.0,
-            color: Colors.white,
-            child: Text('hello'),
-            alignment: Alignment.center,
-            margin: EdgeInsets.all(10.0),
-            padding: EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            verticalDirection: VerticalDirection.down,
+            children: <Widget>[
+              Container(
+                height: 50.0,
+                width: 100.0,
+                color: Colors.white,
+                child: Text('Container 1'),
+                alignment: Alignment.center,
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(10.0),
+              ),
+              Container(
+                height: 50.0,
+                width: 100.0,
+                color: Colors.yellow,
+                child: Text('Container 2'),
+                alignment: Alignment.center,
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(10.0),
+              ),
+              Container(
+                height: 50.0,
+                width: 100.0,
+                color: Colors.white,
+                child: Text('Container 3'),
+                alignment: Alignment.center,
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(10.0),
+              ),
+              Container(
+                width: double.infinity,
+              ),
+            ],
           ),
         ),
       ),
