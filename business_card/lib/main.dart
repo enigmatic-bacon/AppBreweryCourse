3b2592ec591f7448dee3135bcb0690063eee68ac
AlexBismuth <bismuthalex@gmail.com>
2020-07-14 00:07:06 -0700

    Added information boxes

--- business_card/lib/main.dart
+++ business_card/lib/main.dart
@@ -35,10 +35,66 @@ class MyApp extends StatelessWidget {
               Text(
                 'Mobile Developer',
                 style: TextStyle(
-                  fontSize: 25.0,
+                  fontSize: 20.0,
                   color: Colors.teal[100],
                   fontFamily: 'Montserrat',
-                  fontWeight: FontWeight.w200,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 20.0,
+                ),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 25.0,
+                    ),
+                    Text(
+                      '+1 650 387 8701',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal[900],
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 20.0,
+                ),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 25.0,
+                    ),
+                    Text(
+                      'bismuthalex@gmail.com',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal[900],
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
