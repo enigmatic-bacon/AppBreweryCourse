7eaea6040a54e85aba2c515e17e7efb5a45aae8c
AlexBismuth <bismuthalex@gmail.com>
2020-07-17 13:37:47 -0700

    made function to reduct code duplication

--- 5_xylophone/lib/main.dart
+++ 5_xylophone/lib/main.dart
@@ -4,105 +4,41 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey(Color keyColor, int soundNumber) {
+    return Expanded(
+      child: FlatButton(
+        padding: EdgeInsets.all(0.0),
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        child: Container(
+          color: keyColor,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Expanded(
-                child: FlatButton(
-                  padding: EdgeInsets.all(0.0),
-                  onPressed: () {
-                    final player = AudioCache();
-                    player.play('note1.wav');
-                    print('clicked');
-                  },
-                  child: Container(
-                    color: Colors.red,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  padding: EdgeInsets.all(0.0),
-                  onPressed: () {
-                    final player = AudioCache();
-                    player.play('note2.wav');
-                    print('clicked');
-                  },
-                  child: Container(
-                    color: Colors.orange,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  padding: EdgeInsets.all(0.0),
-                  onPressed: () {
-                    final player = AudioCache();
-                    player.play('note3.wav');
-                    print('clicked');
-                  },
-                  child: Container(
-                    color: Colors.yellow,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  padding: EdgeInsets.all(0.0),
-                  onPressed: () {
-                    final player = AudioCache();
-                    player.play('note4.wav');
-                    print('clicked');
-                  },
-                  child: Container(
-                    color: Colors.green,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  padding: EdgeInsets.all(0.0),
-                  onPressed: () {
-                    final player = AudioCache();
-                    player.play('note5.wav');
-                    print('clicked');
-                  },
-                  child: Container(
-                    color: Colors.blue,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  padding: EdgeInsets.all(0.0),
-                  onPressed: () {
-                    final player = AudioCache();
-                    player.play('note6.wav');
-                    print('clicked');
-                  },
-                  child: Container(
-                    color: Colors.pink,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  padding: EdgeInsets.all(0.0),
-                  onPressed: () {
-                    final player = AudioCache();
-                    player.play('note7.wav');
-                    print('clicked');
-                  },
-                  child: Container(
-                    color: Colors.purple,
-                  ),
-                ),
-              ),
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.blue, 5),
+              buildKey(Colors.indigo, 6),
+              buildKey(Colors.purple, 7),
             ],
           ),
         ),
