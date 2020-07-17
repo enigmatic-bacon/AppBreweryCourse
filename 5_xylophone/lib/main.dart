16a0efe68e93db896ebe273694a6573789fb8c2c
AlexBismuth <bismuthalex@gmail.com>
2020-07-17 13:18:47 -0700

    added audio xylophone

--- 5_xylophone/lib/main.dart
+++ 5_xylophone/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,7 +9,102 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  padding: EdgeInsets.all(0.0),
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note1.wav');
+                    print('clicked');
+                  },
+                  child: Container(
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  padding: EdgeInsets.all(0.0),
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note2.wav');
+                    print('clicked');
+                  },
+                  child: Container(
+                    color: Colors.orange,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  padding: EdgeInsets.all(0.0),
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note3.wav');
+                    print('clicked');
+                  },
+                  child: Container(
+                    color: Colors.yellow,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  padding: EdgeInsets.all(0.0),
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note4.wav');
+                    print('clicked');
+                  },
+                  child: Container(
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  padding: EdgeInsets.all(0.0),
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note5.wav');
+                    print('clicked');
+                  },
+                  child: Container(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  padding: EdgeInsets.all(0.0),
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note6.wav');
+                    print('clicked');
+                  },
+                  child: Container(
+                    color: Colors.pink,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  padding: EdgeInsets.all(0.0),
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note7.wav');
+                    print('clicked');
+                  },
+                  child: Container(
+                    color: Colors.purple,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
