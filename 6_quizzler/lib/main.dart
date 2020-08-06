40762aafd6084366eb7a90e4ab9e14859c29313d
AlexBismuth <bismuthalex@gmail.com>
2020-08-05 23:54:41 -0700

    list  of solutions can now be appended

--- 6_quizzler/lib/main.dart
+++ 6_quizzler/lib/main.dart
@@ -25,6 +25,17 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> _answers = [
+    Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+    Icon(
+      Icons.close,
+      color: Colors.red,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -62,6 +73,12 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                setState(() {
+                  _answers.add(Icon(
+                    Icons.close,
+                    color: Colors.green,
+                  ));
+                });
               },
             ),
           ),
@@ -80,21 +97,20 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                setState(() {
+                  _answers.add(
+                    Icon(
+                      Icons.close,
+                      color: Colors.red,
+                    ),
+                  );
+                });
               },
             ),
           ),
         ),
         Row(
-          children: <Widget>[
-            Icon(
-              Icons.check,
-              color: Colors.green, //Test
-            ),
-            Icon(
-              Icons.close,
-              color: Colors.red,
-            ),
-          ],
+          children: _answers,
         )
       ],
     );
