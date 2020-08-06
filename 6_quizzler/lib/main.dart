9442f43248d4ce4e817800834fba8313522b5f49
AlexBismuth <bismuthalex@gmail.com>
2020-08-06 00:21:48 -0700

    rotate through questions

--- 6_quizzler/lib/main.dart
+++ 6_quizzler/lib/main.dart
@@ -25,16 +25,13 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> _answers = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
+  List<Icon> _scorekeeper = [];
+  List<String> _questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
   ];
+  int _questionIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +45,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                _questions[_questionIndex],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -74,7 +71,8 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked true.
                 setState(() {
-                  _answers.add(Icon(
+                  _questionIndex++;
+                  _scorekeeper.add(Icon(
                     Icons.close,
                     color: Colors.green,
                   ));
@@ -98,7 +96,8 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked false.
                 setState(() {
-                  _answers.add(
+                  _questionIndex++;
+                  _scorekeeper.add(
                     Icon(
                       Icons.close,
                       color: Colors.red,
