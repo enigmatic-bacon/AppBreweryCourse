a1083386c963dde8e491604f2925643d41aa16b9
AlexBismuth <bismuthalex@gmail.com>
2020-08-10 19:06:09 -0700

    Initial commit

---
+++ 7_destini/lib/story.dart
@@ -0,0 +1,12 @@
+//COMPLETE: Step 2 - Create a new class called Story.
+class Story {
+  String storyTitle;
+  String choice1Text;
+  String choice2Text;
+
+  Story({this.storyTitle, this.choice1Text, this.choice2Text});
+}
+
+//COMPLETE: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
+
+//COMPLETE: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
